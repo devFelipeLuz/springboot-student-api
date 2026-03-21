b6a7f02a9a844836f7897be6ec0b0ae5da478c96
Felipe Luz <felipetluz@outlook.com>
2026-03-20 22:08:37 -0300

    fix: refactoring SchoolYear Controller's @PreAuthorize sintaxe

--- backend/src/main/java/br/com/backend/controller/SchoolYearController.java
+++ backend/src/main/java/br/com/backend/controller/SchoolYearController.java
@@ -29,21 +29,21 @@ public class SchoolYearController {
     @Operation(summary = "Create schoolYear")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    @PreAuthorize("hasAuthority('ADMIN', 'PROFESSOR')")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'PROFESSOR')")
     public SchoolYearResponseDTO registerSchoolYear(@Valid @RequestBody SchoolYearRequest dto) {
         return service.register(dto);
     }
 
     @Operation(summary = "Find schoolYear by id")
     @GetMapping("/{id}")
-    @PreAuthorize("hasAuthority('ADMIN', 'PROFESSOR')")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'PROFESSOR')")
     public SchoolYearResponseDTO getSchoolYearById(@PathVariable UUID id) {
         return service.findById(id);
     }
 
     @Operation(summary = "List schoolYears")
     @GetMapping
-    @PreAuthorize("hasAuthority('ADMIN', 'PROFESSOR')")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'PROFESSOR')")
     public Page<SchoolYearResponseDTO> getSchoolYears(
             @Parameter(description = "Filter by status active (true or false)")
             @RequestParam
@@ -56,7 +56,7 @@ public class SchoolYearController {
 
     @Operation(summary = "Update schoolYear")
     @PatchMapping("/{id}")
-    @PreAuthorize("hasAuthority('ADMIN', 'PROFESSOR')")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'PROFESSOR')")
     public SchoolYearResponseDTO updateSchoolYear(@PathVariable UUID id,
                                         @Valid @RequestBody SchoolYearRequest dto) {
         return service.update(id, dto);
