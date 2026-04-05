65b8a866031154407539630151c56b431c575520
Felipe Luz <felipetluz@outlook.com>
2026-04-05 02:41:43 -0300

    refactor(repo): promote backend to root and clean project structure

--- frontend/school-management-frontend/src/components/Button/Button.tsx
+++
@@ -1,9 +0,0 @@
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
-
-function Button (props: ButtonProps) {
-    return (
-        <button {...props}>{props.children}</button>
-    );
-}
-
-export default Button;
--- frontend/school-management-frontend/src/components/Input/Input.tsx
+++
@@ -1,9 +0,0 @@
-type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
-
-function Input (props: InputProps) {
-    return (
-        <input {...props} />
-    );
-}
-
-export default Input;
